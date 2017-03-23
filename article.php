f6617b231194438d41cb6ad83ab3f40d2e685320
Unknown <eddyroy2125@gmail.com>
2017-03-23 13:31:40 +0100

    up css

--- article.php
+++ article.php
@@ -2,43 +2,49 @@
 <html>
     <head>
         <meta charset="utf-8">
-		<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+		<link rel="stylesheet" href='css/bootstrap/css/bootstrap.min.css'>
         <link rel="stylesheet" href="css/style.css">
         <title>article blog</title>
 	</head>
 	<body>
 		<main class='container'>
-			<div class='row'>
-				<div class='col-lg-2 col-md-2'>
-					<img src=''/>
+			<div id='title' class='row'>
+				<div class='col-lg-offset-1 col-md-offset-1 col-lg-3 col-md-3'>
+					<img src='images/mel.png'/>
+				</div>
+				<div class='col-lg-offset-1 col-md-offset-1 col-lg-7 col-md-7'>
+					<h4>Bacon Ipsum</h4>
 				</div>
 			</div>
 			<div class='row'>
-				<div class='col-lg-2 col-md-2'>
-					<p>nom</p>
-				</div>
-			</div>
-			<div class='row'>
-				<div class='col-lg-2 col-md-2'>
+				<div id='div_auteur' class='col-lg-offset-1 col-md-offset-1 col-lg-2 col-md-2 text-center'>
+					<p class='nom_auteur'>nom</p>
 					<a href=''><button>Profil</button></a>
 				</div>
 			</div>
 			<article>
-			<h4>Bacon Ipsum</h4>
-			<p>
-			Bacon ipsum dolor amet burgdoggen brisket boudin rump t-bone ham cow kielbasa. Tongue tenderloin cow chicken corned beef, biltong pork chop. Kielbasa t-bone porchetta, jowl capicola andouille ribeye turkey tenderloin venison bresaola flank pork. Sausage hamburger strip steak, capicola short ribs pork tail brisket meatloaf ham hock pastrami swine. Bresaola boudin burgdoggen brisket leberkas capicola pastrami. Picanha meatloaf tongue tenderloin, brisket capicola filet mignon ball tip pig burgdoggen drumstick strip steak jerky beef. Rump doner chicken biltong hamburger fatback tail leberkas.
-
-Leberkas kielbasa beef flank pastrami. Sirloin turkey strip steak prosciutto. Turducken shank boudin prosciutto sirloin drumstick jerky t-bone ribeye spare ribs cupim pork belly ground round. Sausage frankfurter brisket, strip steak sirloin landjaeger tongue burgdoggen salami tri-tip hamburger prosciutto flank pastrami alcatra.
-
-Biltong tenderloin beef landjaeger shoulder ham venison. Alcatra bacon short loin tri-tip cupim biltong filet mignon jerky short ribs chicken. Frankfurter chuck ground round, boudin fatback turducken prosciutto chicken spare ribs leberkas shankle venison rump jowl. Ham hamburger meatloaf pork chop sausage pork belly short loin ball tip short ribs chuck doner turkey. Pancetta leberkas bresaola, landjaeger swine shankle jowl picanha shank short loin pastrami bacon strip steak frankfurter drumstick. Hamburger kevin tenderloin, short loin salami ball tip tri-tip bacon ham hock boudin.
-			</p>
-			</article>
-			<div id="comm">
-				<h4>Commentaires</h4>
-				<p>Nom auteur</p>
-				<p>	Bacon ipsum dolor amet burgdoggen brisket boudin rump t-bone ham cow kielbasa. Tongue tenderloin cow chicken corned beef, biltong pork chop. Kielbasa t-bone porchetta, jowl capicola andouille ribeye turkey tenderloin venison bresaola flank pork. Sausage hamburger strip steak, capicola short ribs pork tail brisket meatloaf ham hock pastrami swine. Bresaola boudin burgdoggen brisket leberkas capicola pastrami. Picanha meatloaf tongue tenderloin, brisket capicola filet mignon ball tip pig burgdoggen drumstick strip steak jerky beef. Rump doner chicken biltong hamburger fatback tail leberkas.
+			
+			<div class='row'>
+				<div class='col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10'>
+					<p>
+					Bacon ipsum dolor amet burgdoggen brisket boudin rump t-bone ham cow kielbasa. Tongue tenderloin cow chicken corned beef, biltong pork chop. Kielbasa t-bone porchetta, jowl capicola andouille ribeye turkey tenderloin venison bresaola flank pork. Sausage hamburger strip steak, capicola short ribs pork tail brisket meatloaf ham hock pastrami swine. Bresaola boudin burgdoggen brisket leberkas capicola pastrami. Picanha meatloaf tongue tenderloin, brisket capicola filet mignon ball tip pig burgdoggen drumstick strip steak jerky beef. Rump doner chicken biltong hamburger fatback tail leberkas.
 					Leberkas kielbasa beef flank pastrami. Sirloin turkey strip steak prosciutto. Turducken shank boudin prosciutto sirloin drumstick jerky t-bone ribeye spare ribs cupim pork belly ground round. Sausage frankfurter brisket, strip steak sirloin landjaeger tongue burgdoggen salami tri-tip hamburger prosciutto flank pastrami alcatra.
-					Biltong tenderloin beef landjaeger shoulder ham venison. Alcatra bacon short loin tri-tip cupim biltong filet mignon jerky short ribs chicken.</p>
+					Biltong tenderloin beef landjaeger shoulder ham venison. Alcatra bacon short loin tri-tip cupim biltong filet mignon jerky short ribs chicken. Frankfurter chuck ground round, boudin fatback turducken prosciutto chicken spare ribs leberkas shankle venison rump jowl. Ham hamburger meatloaf pork chop sausage pork belly short loin ball tip short ribs chuck doner turkey. Pancetta leberkas bresaola, landjaeger swine shankle jowl picanha shank short loin pastrami bacon strip steak frankfurter drumstick. Hamburger kevin tenderloin, short loin salami ball tip tri-tip bacon ham hock boudin.
+					</p>
+				</div>
+			</div>
+			</article>
+			<div id="comm" class='col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10'>
+				<h5>Commentaires</h5>
+				<p>Nom auteur</p>
+				<div class='row'>
+					<div class='nom_auteur'>
+						<p> 
+						Bacon ipsum dolor amet burgdoggen brisket boudin rump t-bone ham cow kielbasa. Tongue tenderloin cow chicken corned beef, biltong pork chop. Kielbasa t-bone porchetta, jowl capicola andouille ribeye turkey tenderloin venison bresaola flank pork. Sausage hamburger strip steak, capicola short ribs pork tail brisket meatloaf ham hock pastrami swine. Bresaola boudin burgdoggen brisket leberkas capicola pastrami. Picanha meatloaf tongue tenderloin, brisket capicola filet mignon ball tip pig burgdoggen drumstick strip steak jerky beef. Rump doner chicken biltong hamburger fatback tail leberkas.
+						Leberkas kielbasa beef flank pastrami. Sirloin turkey strip steak prosciutto. Turducken shank boudin prosciutto sirloin drumstick jerky t-bone ribeye spare ribs cupim pork belly ground round. Sausage frankfurter brisket, strip steak sirloin landjaeger tongue burgdoggen salami tri-tip hamburger prosciutto flank pastrami alcatra.
+						</p>
+					</div>
+				</div>
 			</div>
 		</main>
 	</body>
